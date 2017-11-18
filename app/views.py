be1c41a619462c6c7866e4c7a17b2d9ddc4c904b
vyong <vyong@udel.edu>
2017-11-17 23:20:07 -0500

    Finished Part II: Templates

--- app/views.py
+++ app/views.py
@@ -1,6 +1,25 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'nickname' : 'Miguel'}
+    
+    # Seed data
+    posts = [
+        {
+            'author': {'nickname': 'John'},
+            'body': 'Beatiful day in Portland!'
+        },
+        {
+            'author': {'nickname': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+
+    return render_template(
+        'index.html', 
+        title="Home", 
+        user=user, 
+        posts = posts)
